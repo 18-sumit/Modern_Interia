278227269f70dec4bbca2334cc7b5d9f4553ce89
Sumit Singh <sumitsingh3076@gmail.com>
2024-12-27 00:28:59 +0530

    Router and routes setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
-export default function App() {
+import Home from "./Pages/Home"
+import Services from "./Pages/Services"
+import Contact from "./Pages/Contact"
+import ArtGallery from "./Pages/ArtGallery"
+import { Route, Routes } from "react-router-dom"
+
+
+const App = () => {
   return (
-    <h1 className="text-3xl text-red-800 font-bold underline">
-      Hello world!
-    </h1>
+
+    <div className="px-4 sm:px[5vw] md:px-[7vw] lg:px-[9vw]">
+      <Routes>
+        <Route path="/" element={Home} />
+        <Route path="/services" element={Services} />
+        <Route path="/contact" element={Contact} />
+        <Route path="/art-gallery" element={ArtGallery} />
+      </Routes>
+    </div>
   )
 }
+
+export default App 
