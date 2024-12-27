d5f16cf11e11d7e7e9567b7383aefe4825de90ea
Sumit Singh <sumitsingh3076@gmail.com>
2024-12-28 01:19:32 +0530

    assets file

---
+++ src/assets/assests.js
@@ -0,0 +1,33 @@
+import windows from './windows.jpg'
+import Image1 from './Image1.png'
+import Image2 from './Image2.png'
+import kitchen from './kitchen.jpg'
+import Logo from './Logo.png'
+import sofaset from './sofaset.jpg'
+import wallpainter from './wallpainter.jpg'
+import welding from './welding.jpg'
+import woodgrind from './woodgrind.jpg'
+
+
+
+
+
+
+
+
+
+
+
+
+
+export const assets = {
+    windows,
+    Image1,
+    Image2,
+    kitchen,
+    Logo,
+    sofaset,
+    wallpainter,
+    welding,
+    woodgrind
+}
