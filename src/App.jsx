e26025ba3d2da63c38bdce2d90d4745e7b57cb19
Sumit Singh <sumitsingh3076@gmail.com>
2024-12-29 00:45:40 +0530

    routes done

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Contact from "./Pages/Contact"
 import ArtGallery from "./Pages/ArtGallery"
 import { Route, Routes } from "react-router-dom"
 import Header from "./components/Header"
+import Footer from "./components/Footer"
+// import Hero from "./components/Hero"
 
 
 const App = () => {
@@ -11,12 +13,14 @@ const App = () => {
 
     <div className="px-4 sm:px[5vw] md:px-[7vw] lg:px-[9vw]">
       <Header />
+      {/* <Hero /> */}
       <Routes>
-        <Route path="/" element={Home} />
-        <Route path="/services" element={Services} />
-        <Route path="/contact" element={Contact} />
-        <Route path="/art-gallery" element={ArtGallery} />
+        <Route path="/" element={<Home />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/art-gallery" element={<ArtGallery />} />
       </Routes>
+      <Footer />
     </div>
   )
 }
